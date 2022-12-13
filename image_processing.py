940ebb18dbb06760f00ff361e88d0691307911a5
Josh McArthur <joshua.mcarthur@gmail.com>
2022-12-14 10:31:20 +1300

    Quote filename to handle files with parentheses

--- image_processing.py
+++ image_processing.py
@@ -1,3 +1,6 @@
+import os
+from shlex import quote
+
 from log import print_run_msg
 
 
@@ -5,29 +8,29 @@ def generate_thumbnail(filename):
         # Create thumbnails used on search screens
         # Some images have a 1px border that looks bad in search results
         # Not all do - but we can safely trim 1px off all images
-        shave_cmd = "mogrify -shave 1x1 assets/" + filename
+        shave_cmd = "mogrify -shave 1x1 assets/" + quote(filename)
         print_run_msg(shave_cmd)
         os.system(shave_cmd)
 
         # Then we make thumbnails of the border-free images
         create_thumbnail_cmd = "convert -resize x92 assets/" + \
-            filename + " assets/50." + filename
+            quote(filename) + " assets/50." + quote(filename)
         print_run_msg(create_thumbnail_cmd)
         os.system(create_thumbnail_cmd)
 
 def resize_image(filename):
         # Resize images larger than 600x600 down using mogrify from imagemagick
-        cmd = "mogrify -resize '600x600>' assets/" + filename
+        cmd = "mogrify -resize '600x600>' assets/" + quote(filename)
         print_run_msg(cmd)
         os.system(cmd)
 
 def reduce_colour_depth(filename):
-        recolor_cmd = "convert -colors 4 assets/" + filename + " assets/" + filename
+        recolor_cmd = "convert -colors 4 assets/" + quote(filename) + " assets/" + quote(filename)
         print_run_msg(recolor_cmd)
         os.system(recolor_cmd)
 
 def optimise_image(filename):
-        optipng_cmd = "optipng -quiet assets/" + filename
+        optipng_cmd = "optipng -quiet assets/" + quote(filename)
         print_run_msg(optipng_cmd)
         os.system(optipng_cmd)
 
