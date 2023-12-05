bf80ee122ba4298b1ff8f8222804ebe44e36eb69
Josh McArthur <joshua.mcarthur@gmail.com>
2023-12-05 15:47:51 +1300

    Add support for a --skip-assets option which will not download and process sign media

--- build-assets-from-signbank.py
+++ build-assets-from-signbank.py
@@ -10,6 +10,7 @@ from log import print_run_msg
 parser = OptionParser()
 parser.add_option("-c", action="store_true", dest="cleanup",
                   help="clean up files after execution")
+parser.add_option("--skip-assets", action="store_true", help="Export Signbank data, but not supporting media", dest="skip_assets")
 
 (options, args) = parser.parse_args()
 
@@ -19,6 +20,7 @@ dat_file_filename = "nzsl.dat"
 database_filename = 'nzsl.db'
 assets_folder = 'signbank-assets'
 pictures_folder = 'assets'
+download = not options.skip_assets
 
 print("Step 1: Fetching the latest signs from Signbank")
 signbank.fetch_gloss_export_file(filename)
@@ -32,16 +34,17 @@ signbank.write_sqlitefile(data, database_filename)
 print("Step 4: Fetching assets from signbank")
 signbank.fetch_gloss_asset_export_file(video_filename)
 asset_data = signbank.parse_signbank_csv(video_filename)
-signbank.fetch_gloss_assets(asset_data, database_filename, assets_folder)
+signbank.fetch_gloss_assets(asset_data, database_filename, assets_folder, download=download)
 
 print("Step 4: Write out nzsl.dat for Android")
 signbank.write_datfile(database_filename, dat_file_filename)
 
-print("Step 5: Merge images together into one folder")
-signbank.copy_images_to_one_folder(assets_folder, pictures_folder)
+if download:
+    print("Step 5: Merge images together into one folder")
+    signbank.copy_images_to_one_folder(assets_folder, pictures_folder)
 
-print("Step 6: Prepare images for distribution")
-image_processing.process_images(pictures_folder)
+    print("Step 6: Prepare images for distribution")
+    image_processing.process_images(pictures_folder)
 
 if options.cleanup:
     print("Step 7: Cleanup")
